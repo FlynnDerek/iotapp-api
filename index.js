93efe029afae426fb6d7c1dd8cd90df4718c1f95
Derek Flynn <47580022+FlynnDerek@users.noreply.github.com>
2020-07-15 06:45:57 -0400

    added https support

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var axios = require("axios") 
+var axios = require("axios")
 
 module.exports = {
 
   // getPublicChain() method returns all blockchain data except private transactions. 
   // Use this method to perform analysis, block and node validation. 
   getPublicChain () {
-        return axios.get(`http://192.241.155.14:5000/test`)
+        return axios.get(`https://iotappbe.com:5001/test`)
         .then((response) => {
           let chain = response.data;
           return chain.slice().reverse()
@@ -18,7 +18,7 @@ module.exports = {
     // getPrivateChain() method returns all private blockchain data for a given write key.
     // Use this method to get and display all transactions signed to your dapp. 
   getPrivateChain (key1, key2) {
-      return axios.post('http://192.241.155.14:5000/passkeys', {
+      return axios.post('https://iotappbe.com:5001/passkeys', {
         privKey1: key1,
         privKey2: key2,
       })
@@ -34,7 +34,7 @@ module.exports = {
   //getLastBlock() method returns the last block in the private chain for a given write key.
   // Use this method to get and display the latest incoming data for your dapp. 
   getLatestTx (key1, key2) {
-    return axios.post('http://192.241.155.14:5000/passkeys', {
+    return axios.post('https://iotappbe.com:5001/passkeys', {
       privKey1: key1,
       privKey2: key2,
     })
@@ -46,5 +46,4 @@ module.exports = {
       console.log("API error");
     }) 
   }
-
 }
